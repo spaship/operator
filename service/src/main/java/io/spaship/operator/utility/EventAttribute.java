3efe09398e28ccff62bde309a6b3e350d221ab13
arkaprovo <apb@live.in>
2021-07-30 18:51:21 +0530

    added fail event for component refresh and env attribute support for separate events

--- service/src/main/java/io/spaship/operator/utility/EventAttribute.java
+++ service/src/main/java/io/spaship/operator/utility/EventAttribute.java
@@ -9,11 +9,14 @@ public interface EventAttribute {
     String CODE="CODE~";
     String TRACE_ID="traceId~";
     String TIMESTAMP="timestamp~";
+    String ERROR="error~";
+    String ENVIRONMENT="refresh-env~";
     enum EventCode{
         WEBSITE_CREATE_INIT,
         WEBSITE_CREATE_OR_UPDATE_INIT,
         WEBSITE_REFRESH_COMPONENT_INIT,
         WEBSITE_REFRESH_COMPONENT,
+        WEBSITE_REFRESH_COMPONENT_FAILED,
         WEBSITE_CREATE,
         WEBSITE_UPDATE,
         WEBSITE_DELETE,
